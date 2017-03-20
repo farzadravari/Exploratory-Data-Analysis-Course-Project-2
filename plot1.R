84f5fccb3c4965fe219de6c32b7e5f636e504116
farzadravari <farzadkr@yahoo.com>
2017-03-20 17:10:41 +0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,34 +1,28 @@
-getwd()
-[1] "C:/Documents and Settings/MSC/Desktop/Dr.Farzad/Data Science/Course 4"
-> setwd("C:/Documents and Settings/MSC/Desktop/Dr.Farzad/Data Science/Course 4/datastore")
 > getwd()
-[1] "C:/Documents and Settings/MSC/Desktop/Dr.Farzad/Data Science/Course 4/datastore"
+[1] "C:/Users/farza/OneDrive/Documents"
+> setwd("C:/Users/farza/Desktop/Data Science/Course 4/Project 2")
+> getwd()
+[1] "C:/Users/farza/Desktop/Data Science/Course 4/Project 2"
+> if(!file.exists("./dataStore")){dir.create("./dataStore")}
+> setwd("C:/Users/farza/Desktop/Data Science/Course 4/Project 2/datastore")
+> getwd()
+[1] "C:/Users/farza/Desktop/Data Science/Course 4/Project 2/datastore"
+> # Read data files
+> # read national emissions data
 > NEI <- readRDS("./summarySCC_PM25.rds")
 > SCC <- readRDS("./Source_Classification_Code.rds")
-> number.add.width<-800 
-> number.add.height<-800  
-> require(dplyr)
-Loading required package: dplyr
-
-Attaching package: ‘dplyr’
-
-The following objects are masked from ‘package:stats’:
-
-    filter, lag
-
-The following objects are masked from ‘package:base’:
-
-    intersect, setdiff, setequal, union
-
 > install.packages("dplyr")
-Error in install.packages : Updating loaded packages
-> install.packages("dplyr")
-Warning in install.packages :
-  package ‘dplyr’ is in use and will not be installed
+Installing package into ‘C:/Users/farza/OneDrive/Documents/R/win-library/3.3’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.3/dplyr_0.5.0.zip'
+Content type 'application/zip' length 2557014 bytes (2.4 MB)
+downloaded 2.4 MB
+package ‘dplyr’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+	C:\Users\farza\AppData\Local\Temp\Rtmp8KrfMm\downloaded_packages
 > library(dplyr)
 > total.emissions <- summarise(group_by(NEI, year), Emissions=sum(Emissions))
-> clrs <- c("red", "green", "blue", "yellow")
-> x1<-barplot(height=total.emissions$Emissions/1000, names.arg=total.emissions$year,
-+             xlab="years", ylab=expression('total PM'[2.5]*' emission in kilotons'),ylim=c(0,8000),
-+             main=expression('Total PM'[2.5]*' emissions at various years in kilotons'),col=clrs)
-> text(x = x1, y = round(total.emissions$Emissions/1000,2), label = round(total.emissions$Emissions/1000,2), pos = 3, cex = 0.8, col = "black")
+> clrs <- c("red", "purple", "blue", "green")
+> x1<-barplot(height=total.emissions$Emissions/1000, names.arg=total.emissions$year,xlab="years", ylab=expression('total PM'[2.5]*' emission in kilotons'),ylim=c(0,8000),main=expression('Total PM'[2.5]*' emissions at various years in kilotons'),col=clrs)
+> text(x = x1, y = round(total.emissions$Emissions/1000,2), label = round(total.emissions$Emissions/1000,2), pos = 3, cex = 0.8, col = "red")
